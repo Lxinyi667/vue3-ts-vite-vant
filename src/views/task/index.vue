f5ff30d83a02bccf945d60eac408ff77dd2b2438
nameVal <eamil@qq.com>
2023-12-06 09:36:15 +0800

    底部导航公共组件

---
+++ src/views/task/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import FooterTabbar from '@/components/FooterTabbar.vue';
+</script>
+
+<template>
+    <FooterTabbar></FooterTabbar>
+</template>
+<style scoped>
+
+</style>
